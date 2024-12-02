ca45c30dcd19a7d9bcdf6ee4ffc79e67c8732075
Matthew Dixon <m.dixon5@outlook.com>
2024-12-02 09:59:52 +0000

    Install and setup crispy forms

--- film_archive/settings.py
+++ film_archive/settings.py
@@ -57,11 +57,14 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     
-    'cloudinary_storage',
-    'cloudinary',
-    
     #Apps
     'home',
+
+    #Other
+    'cloudinary_storage',
+    'cloudinary',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 SITE_ID = 1
@@ -77,6 +80,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 ROOT_URLCONF = 'film_archive.urls'
 
@@ -95,6 +101,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': {
+                'crispy_forms.templatetags.crispy_forms_tags'
+                'crispy_forms.templatetags.crispy_forms_field'
+            }
         },
     },
 ]
