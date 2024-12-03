92a22006f86db270180a5ed334561d16f06dfa6d
Matthew Dixon <m.dixon5@outlook.com>
2024-12-03 13:45:41 +0000

    Add forms model and view to reviews app

---
+++ reviews/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from djrichtextfield.widgets import RichTextWidget
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    """ Form to create a Review """
+    class Meta:
+        model = Review
+        fields = ['film_title', 'film_image', 'film_rating', 'summary', 'review', 'date_watched']
+
+        review = forms.CharField(widget=RichTextWidget())
+
+        widget = {
+            'summary': forms.Textarea(attrs={'rows': 3}),
+        }
+
+        labels = {
+            'film_title': 'Film Title',
+            'film_image': 'Film Image',
+            'film_rating': 'Rating',
+            'summary': 'Review Summary',
+            'review': 'Review',
+            'date_watched': 'Date Watched',
+        }
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+
+from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
+class AddReview(CreateView):
+    """ Add review view """
+    template_name = 'reviews/add_review.html'
+    model = Review
+    form_class = ReviewForm
+    success_url = '/reviews/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddReview, self).form_valid(form)
