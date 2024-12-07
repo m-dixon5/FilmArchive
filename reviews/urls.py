1b32d07d4d92a072d87d57b6bf567cdc42ff1f2e
Matthew Dixon <m.dixon5@outlook.com>
2024-12-07 11:24:37 +0000

    Add confirm delete template code and link up urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddReview, UserReviews, ReviewDetail
+from .views import AddReview, UserReviews, ReviewDetail, DeleteReview
 
 
 urlpatterns = [
-    path("", AddReview.as_view(), name="add_review"),
-    path("reviews/", UserReviews.as_view(), name="reviews"),
-    path("<slug:pk>/", ReviewDetail.as_view(), name="review_detail")
+    path("add/", AddReview.as_view(), name="add_review"),
+    path("", UserReviews.as_view(), name="reviews"),
+    path("<slug:pk>/", ReviewDetail.as_view(), name="review_detail"),
+    path("delete/<slug:pk>/", DeleteReview.as_view(), name="delete_review"),
 ]
