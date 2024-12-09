b7865c99f190981fa34f9de0945e5c7986ddcb55
Matthew Dixon <m.dixon5@outlook.com>
2024-12-09 22:57:49 +0000

    Add form for adding film to wach later list

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Profile
+from .models import Profile, WatchLater
+from djrichtextfield.widgets import RichTextWidget
 
 
 class ProfileForm(forms.ModelForm):
@@ -10,3 +11,22 @@ class ProfileForm(forms.ModelForm):
         fields = ["image", "bio"]
 
         labels = {"image": "Avatar", "bio": "Bio"}
+
+
+class WatcherLaterForm(forms.ModelForm):
+    """ Form to add a film to watch later list """
+
+    class Meta:
+        model = WatchLater
+        fields = [
+            "film_title",
+            "film_image",
+        ]
+
+        review = forms.CharField(widget=RichTextWidget())
+
+        labels = {
+            "film_title": "Film Title",
+            "film_image": "Film Image",
+        }
+
