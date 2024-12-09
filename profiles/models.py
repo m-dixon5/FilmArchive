433560eb54bf216c6f04793c651c809198301b5d
Matthew Dixon <m.dixon5@outlook.com>
2024-12-09 22:52:03 +0000

    Add model for comments

--- profiles/models.py
+++ profiles/models.py
@@ -31,3 +31,25 @@ def create_user_profile(instance, created, **kwargs):
     """Create or update the user profile"""
     if created:
         Profile.objects.create(user=instance)
+
+
+class WatchLater(models.Model):
+    """
+    Model for adding films to watch later list
+    """
+
+    user = models.ForeignKey(
+        User, related_name="review_author", on_delete=models.CASCADE
+    )
+    film_title = models.CharField(max_length=100, null=False, blank=False)
+    film_image = ResizedImageField(
+        size=(300, 450),
+        quality=75,
+        upload_to="reviews/",
+        force_format="WEBP",
+        blank=False,
+        null=False,
+    )
+
+    def __str__(self):
+        return str(self.title)
