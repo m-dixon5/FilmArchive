8484524d9be8eccb46df8b96d2e092874c79cb46
Matthew Dixon <m.dixon5@outlook.com>
2024-12-10 16:41:48 +0000

    Add class to view comments in admin page

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Review
+from .models import Review, Comment
 
 
 # Register your models here.
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ("user", "film_title", "summary", "review", "posted_on")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "review", "body", "created_on")
